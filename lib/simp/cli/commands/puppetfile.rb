d50e870fb72525247a54ab9b3443e0f7ec0e9995
Chris Tessmer <chris.tessmer@onyxpoint.com>
2019-03-12 01:24:44 +0000

    Flesh out code and tests

--- lib/simp/cli/commands/puppetfile.rb
+++ lib/simp/cli/commands/puppetfile.rb
@@ -5,12 +5,38 @@ class Simp::Cli::Commands::Puppetfile < Simp::Cli::Commands::Command
   # Load sub-commands
   def initialize
     @sub_commands = {}
-    subcmd_files = Dir.glob(File.expand_path('*.rb',__dir__)).sort_by(&:to_s)
+    subcmd_files = Dir.glob(File.expand_path('puppetfile/*.rb',__dir__)).sort_by(&:to_s)
     subcmd_files.each { |file| require file }
-
     Simp::Cli::Commands::Puppetfile::constants.each do |constant|
       cmd = constant.to_s.downcase
       @sub_commands[cmd] = Simp::Cli::Commands::Puppetfile.const_get(constant)
+    end
+  end
+
+  # @return [String] list of subcommands and their descriptions
+  def subcommand_list
+    max_chars = @sub_commands.keys.map{|x| x.size}.max
+    @sub_commands.map do |cmd_name,cmd|
+      "    #{cmd_name.ljust(max_chars + 4)} #{cmd.description}"
+    end.join("\n")
+  end
+
+  # Run sub-command or provide help
+  def run(args)
+    sub_args = parse_command_line(args)
+    cmd = sub_args.shift
+    if @sub_commands.keys.include?(cmd)
+      puts "\n=== PUPPETFILE COMMAND: '#{cmd}'\n\n"
+      sub_cmd = @sub_commands[cmd].new
+      sub_cmd.run(sub_args)
+    else
+      if cmd || args.size > 0
+        warn("Did not recognize '#{cmd} #{args.join(' ')}'")
+      else
+        warn("Did not provide sub-command")
+      end
+
+      help
     end
   end
 
@@ -18,6 +44,7 @@ class Simp::Cli::Commands::Puppetfile < Simp::Cli::Commands::Command
   def help
     parse_command_line( [ '--help' ] )
   end
+
 
   # Parse command-line options for the command
   #   (Leaves sub-command options alone)
@@ -50,27 +77,4 @@ class Simp::Cli::Commands::Puppetfile < Simp::Cli::Commands::Command
     opt_parser.order!(args)
   end
 
-  # @return [String] list of subcommands and their descriptions
-  def subcommand_list
-    max_chars = @sub_commands.keys.map{|x| x.size}.max
-    @sub_commands.map do |cmd_name,cmd|
-      "    #{cmd_name.ljust(max_chars + 4)} #{cmd.description}"
-    end.join("\n")
-  end
-
-  # Run sub-command or provide help
-  def run(args)
-    sub_args = parse_command_line(args)
-    cmd = sub_args.shift
-    if @sub_commands.keys.include?(cmd)
-      puts "\n=== PUPPETFILE COMMAND: '#{cmd}'\n\n"
-      sub_cmd = @sub_commands[cmd].new
-      sub_cmd.run(sub_args)
-    else
-      if cmd || args.size > 0
-      end
-
-      help
-    end
-  end
 end
--- lib/simp/cli/commands/puppetfile/generate.rb
+++ lib/simp/cli/commands/puppetfile/generate.rb
@@ -1,26 +1,70 @@
 require 'simp/cli/commands/command'
+require 'simp/cli/commands/puppetfile'
+require 'json'
+
+class PuppetModuleRpmRepoScanner
+  def initialize
+    @simp_rpm_module_path = '/usr/share/simp/modules'
+    @simp_git_repo_path   = '/usr/share/simp/git/puppet_modules'
+  end
+
+  def pupmod_rpm_list
+    rpm_modules = `rpm -qa`.strip.split("\n").grep(%r{^pupmod-})
+    fail('ERROR: No "pupmod-" RPMs found on system.') if rpm_modules.empty?
+    rpm_modules
+  end
+
+  def metadata(mdj_file)
+    fail("ERROR: No metadata.json at '#{mdj_file}'") unless File.exists?(mdj_file)
+    json = File.read(mdj_file)
+    JSON.parse(json)
+  end
+
+  # Search RPM manifest for the module's top-level metadata.json
+  def metadata_json_path_from_rpm(rpm)
+    mdj_files = `rpm -ql #{rpm}`.strip.split("\n").grep(
+      %r{^#{@simp_rpm_module_path}/[^/]+/metadata\.json}
+    )
+    unless mdj_files.size == 1
+      fail("ERROR: Expected one top-level metadata.json file for '#{rpm}':" \
+        "\n\n#{mdj_files.map{|x| "  - #{x}" }}\n\n" )
+    end
+    mdj_files.first
+  end
+
+  def local_git_repo_path(name)
+    repo_path = File.join(@simp_git_repo_path,"#{name}.git")
+    unless File.directory?(repo_path)
+      fail("ERROR: Cannot find local git repository at '#{repo_path}'")
+    end
+    repo_path
+  end
+
+  def run
+    result = {}
+    pupmod_rpm_list.each do |rpm|
+      mdj_file = metadata_json_path_from_rpm(rpm)
+      data = metadata(mdj_file)
+      name = data['name'] || fail("ERROR: could not read name from module metadata ('#{mdj_file}')")
+      git_repo = local_git_repo_path(name)
+      require 'pry'; binding.pry
+    end
+    result
+  end
+
+  def scan_module
+    # For each module:
+    # get variables
+    forge_org   = nil
+    module_name = nil
+    # check: do RPM files exist? || fail()
+    module_version = nil # metadata.json
+    # check: local git repo exists || fail()
+    puts forge_org, module_name, module_version
+  end
+end
 
 class Simp::Cli::Commands::Puppetfile::Generate < Simp::Cli::Commands::Command
-  class PuppetModuleRpmRepoScanner
-    def initialize
-      @simp_rpm_module_path = '/usr/share/simp/modules'
-      @simp_git_repo_path = '/var/simp/git_repos'
-    end
-
-    def run
-    end
-
-    def scan_module
-      # For each module:
-      # get variables
-      forge_org   = nil
-      module_name = nil
-      # check: do RPM files exist? || fail()
-      module_version = nil # metadata.json
-      # check: local git repo exists || fail()
-      puts forge_org, module_name, module_version
-    end
-  end
 
   def self.description
     'Generate a Puppetfile from SIMP RPM-managed local git repos'
@@ -63,6 +107,7 @@ class Simp::Cli::Commands::Puppetfile::Generate < Simp::Cli::Commands::Command
   def run(args)
     parse_command_line(args)
     pmrr_scanner = PuppetModuleRpmRepoScanner.new
+    pmrr_scanner.run
   end
 end
 
